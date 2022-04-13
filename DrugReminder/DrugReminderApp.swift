4487b27579e4f067e33f2dcecfd82b4e4758f03f
popaka93 <popaka93@gmail.com>
2022-04-13 14:09:59 -0700

    core services like scanning images and translating to text

---
+++ DrugReminder/DrugReminderApp.swift
@@ -0,0 +1,18 @@
+//
+//  DrugReminderApp.swift
+//  DrugReminder
+//
+//  Created by Anh Nguyen on 8/30/21.
+//
+
+import SwiftUI
+
+@main
+struct DrugReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
---
+++ DrugReminder/ScannerView.swift
@@ -0,0 +1,57 @@
+//
+//  ScannerView.swift
+//  DrugReminder
+//
+//  Created by Anh Nguyen on 2/15/22.
+//
+
+import VisionKit
+import SwiftUI
+
+import VisionKit
+import SwiftUI
+
+struct ScannerView: UIViewControllerRepresentable {
+    private let completionHandler: ([String]?) -> Void
+     
+    init(completion: @escaping ([String]?) -> Void) {
+        self.completionHandler = completion
+    }
+     
+    typealias UIViewControllerType = VNDocumentCameraViewController
+     
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ScannerView>) -> VNDocumentCameraViewController {
+        let viewController = VNDocumentCameraViewController()
+        viewController.delegate = context.coordinator
+        return viewController
+    }
+     
+    func updateUIViewController(_ uiViewController: VNDocumentCameraViewController, context: UIViewControllerRepresentableContext<ScannerView>) {}
+     
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(completion: completionHandler)
+    }
+     
+    final class Coordinator: NSObject, VNDocumentCameraViewControllerDelegate {
+        private let completionHandler: ([String]?) -> Void
+         
+        init(completion: @escaping ([String]?) -> Void) {
+            self.completionHandler = completion
+        }
+         
+        func documentCameraViewController(_ controller: VNDocumentCameraViewController, didFinishWith scan: VNDocumentCameraScan) {
+            print("Document camera view controller did finish with ", scan)
+            let recognizer = TextRecognizer(cameraScan: scan)
+            recognizer.recognizeText(withCompletionHandler: completionHandler)
+        }
+         
+        func documentCameraViewControllerDidCancel(_ controller: VNDocumentCameraViewController) {
+            completionHandler(nil)
+        }
+         
+        func documentCameraViewController(_ controller: VNDocumentCameraViewController, didFailWithError error: Error) {
+            print("Document camera view controller did finish with error ", error)
+            completionHandler(nil)
+        }
+    }
+}
